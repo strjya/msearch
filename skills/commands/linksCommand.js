77de72a434e765cdc264124028ed0d4c7d1b1647
Federico Aleotti <Fede@BlackBook.local>
2018-04-06 20:08:12 +0200

    step 1

--- skills/commands/linksCommand.js
+++ skills/commands/linksCommand.js
@@ -3,4 +3,13 @@ var mysql = require('mysql');
 module.exports = (bot, message) => {
     bot.replyPrivate(message, "Solo un momento...")
     bot.replyPrivateDelayed(message, "e due")
+    var connection = mysql.createConnection({
+                  user: 'root',
+                  password: 'ThisIsSAAMComo!',
+                  server: 'sword.academy',
+                  database: 'Sql1001475_3'
+                })
+      // connect to your database
+      connection.connect()
+      bot.replyPrivateDelayed(message, "connesso")
     }
