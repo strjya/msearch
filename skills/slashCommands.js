28c4bc5dfcee476c9ac6ccedb5d94271fe4dce8d
Federico Aleotti <Fede@BlackBook.local>
2018-04-06 16:42:17 +0200

    still now wirking

--- skills/slashCommands.js
+++ skills/slashCommands.js
@@ -5,8 +5,9 @@ module.exports = function(controller) {
       switch (message.command) {
         case '/prova':
           bot.replyPrivate(message, "at least this works")
-          links(bot, message)
           bot.replyPrivate(message, "at least this works2")
+          links(bot, message)
+          bot.replyPrivate(message, "at least this works3")
           break
         default:
           bot.replyPrivate(message,'Chiedo venia, sembra che questo comando esuli dalle mie capacità attuali')
