f878a4ad64053fb16de140da826c82ee75d75c9e
Federico Aleotti <Fede@BlackBook.local>
2018-04-07 21:02:05 +0200

    ...

--- skills/commands/linksCommand.js
+++ skills/commands/linksCommand.js
@@ -11,18 +11,19 @@ module.exports = (bot, message) => {
       connection.connect();
       connection.query("SELECT status, course FROM people WHERE slackid = '"+message.user+"'", function(error, results) {
         bot.replyPrivateDelayed(message,"vjvjfj")
-        if (results.length > 0) {
-          if (results[0].status !== 'Fallen' && results[0].status !== 'Rifiutato' && results[0].course === 'Adulti') {
-            auth = results[0].status
-            key = generateKey();
-            connection.query("UPDATE people SET accesskey = '"+key+"' , expiration = DATE_ADD(NOW(), INTERVAL 1 HOUR) WHERE slackid = "+message.user);
-          } else
-            auth = false;
-        }
-        else auth = false;
-        var response = createMessage(auth, key);
-        bot.replyPrivateDelayed(message,response)
       })
+      if (results.length > 0) {
+        if (results[0].status !== 'Fallen' && results[0].status !== 'Rifiutato' && results[0].course === 'Adulti') {
+          auth = results[0].status
+          key = generateKey();
+          //connection.query("UPDATE people SET accesskey = '"+key+"' , expiration = DATE_ADD(NOW(), INTERVAL 1 HOUR) WHERE slackid = "+message.user);
+        } else
+          auth = false;
+      }
+      else auth = false;
+      var response = createMessage(auth, key);
+      bot.replyPrivateDelayed(message,response)
+
       connection.end();
 
     function createMessage(auth, key) {
