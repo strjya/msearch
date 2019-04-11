830263782468eeaefb97d899e98ba0c3a21a9e43
Federico Aleotti <Fede@BlackBook.local>
2019-04-11 09:32:31 +0200

    test

--- skills/basic_commands.js
+++ skills/basic_commands.js
@@ -46,9 +46,9 @@ module.exports = function(controller) {
     controller.hears(['^ciao(.*)','^buongiorn(.*)','^buon giorn(.*)','^buonaser(.*)','^buona ser(.*)','^ave (.*)', '^ave', '^ave!'], 'direct_message,direct_mention,mention', function(bot, message) {
         let hours = new Date().getHours()
         if (hours < 3 || hours > 18)
-          bot.reply(message,"Buonasera, <@"+message.user+">" )
+          bot.reply(message,"Buonasera, <@"+message.channel+">" )
         else
-          bot.reply(message,"Buongiorno, <@"+message.user+">" )
+          bot.reply(message,"Buongiorno, <@"+message.channel+">" )
     });
 
     controller.hears(['banca','bancari.','coordinate', 'iban', 'pagar', 'pagament' ], 'direct_message,direct_mention,mention', function(bot, message) {
