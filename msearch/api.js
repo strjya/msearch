17d84186ecf0386432df60a99c2c11115b6a6cca
Federico Aleotti <Fede@iMac-di-BlackBytes.local>
2020-01-20 12:34:38 +0100

    minor fix

--- msearch/api.js
+++ msearch/api.js
@@ -10,7 +10,7 @@ router.use(function(req, res, next) {
   next();
 });
 
-router.get('/getTreatisesList', function(req, res) {
+router.post('/getTreatisesList', function(req, res) {
   //let password = req.body.params.password
   //let user = req.body.params.user.trim().toLowerCase()
   database.query("SELECT author, title, year FROM treatises WHERE available = 1",
